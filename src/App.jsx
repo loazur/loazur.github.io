2c283b2fba022ff809b445eb3c2bfca1dd689ce4
loazur <clement.bounaix@gmail.com>
2025-11-26 22:29:41 +0100

    Site fonctionnel, manque plus que le développer

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,14 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>  
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
 
-      <h1>React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Compteur: {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        
-      </p>
+      <h1>Site en pleine refonte...</h1>
+      <h2>Revenez plus tard.</h2>
     </>
   )
 }
